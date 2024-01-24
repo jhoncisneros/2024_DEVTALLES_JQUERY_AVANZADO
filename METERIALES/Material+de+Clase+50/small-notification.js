e5cb7729e105bb9bc8edb162db82ae3816dd8c5c
Jhon Cisneros <jhoncisneros@gmail.com>
2024-01-23 19:35:20 -0500

    SUBIENDO CURSO

---
+++ METERIALES/Material+de+Clase+50/small-notification.js
@@ -0,0 +1,23 @@
+
+
+(function($){
+
+  $(document).ready(function(){
+    var contenido = '<div id="small-top-left-container"></div>';
+    $('body').append(contenido);
+  });
+
+
+  $.smallBox = function(){
+
+    ajustes = {
+      titulo: "Hola Mundo",
+      contenido: "Saludos, soy un plugin!",
+      img: 'assets/img/ball.png'
+    }
+
+
+  }
+
+
+})(jQuery);
